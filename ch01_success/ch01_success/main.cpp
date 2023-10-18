f0a21be28f8f50e6c29508c22d04ccfdd8221400
CelineWW <wzhj.ce@gmail.com>
2023-10-18 09:33:06 -0700

    add ch01

---
+++ ch01_success/ch01_success/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ch01_success
+//
+//  Created by Celine Wang on 8/29/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Success!" << endl;
+    return 0;
+}
+/*
+ * Auther: Celine Wang
+ */
