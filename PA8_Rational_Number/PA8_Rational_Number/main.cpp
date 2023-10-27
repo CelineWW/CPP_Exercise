492b45018cba2e9e19754426b3a49f0c2b9302e7
CelineWW <wzhj.ce@gmail.com>
2023-10-27 00:38:28 -0700

    update PA8 rational number

--- PA8_Rational_Number/PA8_Rational_Number/main.cpp
+++ PA8_Rational_Number/PA8_Rational_Number/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 struct Ratio {
@@ -53,14 +54,30 @@ bool validate_format(string input, bool& valid_ratio, string& p, string& q, Rati
     }
     int slash_index = input.find('/');
     int sign_index = input.find('-');
+    if ((sign_index != -1) && (sign_index != 0) && (sign_index != slash_index + 1)){
+        valid_ratio = false;
+    }
     if (slash_index > -1) {
-        // rest of digits must be numbers
+        // rest of digits must be numbers or minus sign
         for (char c: input) {
             if ((!isdigit(c)) && (c != '/') && (c != '-')) {
                 valid_ratio = false;
             }
         }
-        if (valid_ratio && ((sign_index == -1) ||(sign_index == 0))) {
+        if (valid_ratio && ((sign_index == -1) ||(sign_index == 0))) {  // if numerator contains minus sign
+            // extract number
+            p = input.substr(0, slash_index);
+            q = input.substr(slash_index+1);
+            ratio.numerator = stoi(p);
+            ratio.denominator = stoi(q);
+            if (ratio.denominator == 0) {
+                valid_ratio = false;
+            }
+            else {
+                valid_ratio = true;
+            }
+        }
+        if (valid_ratio && ((sign_index == slash_index + 1) ||(sign_index == 0))) {  // if denominator contains minus sign
             // extract number
             p = input.substr(0, slash_index);
             q = input.substr(slash_index+1);
@@ -75,6 +92,7 @@ bool validate_format(string input, bool& valid_ratio, string& p, string& q, Rati
         }
     }
     
+    // integer numbers is also allowed, default denominator is 1
     if (is_digit(input)) {
         valid_ratio = true;
     }
@@ -97,6 +115,18 @@ int find_gcd(int num1, int num2){
     return gcd;
 }
 
+// reduce_form function definition
+Ratio reduce_form(Ratio ratio){
+    Ratio simplified;
+    simplified.numerator = ratio.numerator / find_gcd(ratio.numerator, ratio.denominator);
+    simplified.denominator = ratio.denominator / find_gcd(ratio.numerator, ratio.denominator);
+    if (simplified.denominator < 0){
+        simplified.numerator = - simplified.numerator;
+        simplified.denominator = - simplified.denominator;
+    }
+    return simplified;
+}
+
 // perform_addition function definition
 Ratio perform_addition(Ratio ratio1, Ratio ratio2) {
     Ratio add;
@@ -106,6 +136,10 @@ Ratio perform_addition(Ratio ratio1, Ratio ratio2) {
     int denominator_add = lcm_add/find_gcd(lcm_add, sum_add);
     add.numerator = numerator_add;
     add.denominator = denominator_add;
+    if (add.denominator < 0) {
+        add.numerator = - add.numerator;
+        add.denominator = - add.denominator;
+    }
     return add;
 }
 
@@ -118,6 +152,10 @@ Ratio perform_subtraction(Ratio ratio1, Ratio ratio2) {
     int denominator_sub = lcm_sub/find_gcd(lcm_sub, diff_sub);
     sub.numerator = numerator_sub;
     sub.denominator = denominator_sub;
+    if (sub.denominator < 0) {
+        sub.numerator = - sub.numerator;
+        sub.denominator = - sub.denominator;
+    }
     return sub;
 }
 
@@ -130,6 +168,10 @@ Ratio perform_multiplication(Ratio ratio1, Ratio ratio2) {
     int denominator_mul = mul_den/find_gcd(mul_den, mul_num);
     mul.numerator = numerator_mul;
     mul.denominator = denominator_mul;
+    if (mul.denominator < 0) {
+        mul.numerator = - mul.numerator;
+        mul.denominator = - mul.denominator;
+    }
     return mul;
 }
 
@@ -146,7 +188,10 @@ Ratio perform_division(Ratio ratio1, Ratio ratio2) {
     int denominator_div = div_den/find_gcd(div_den, div_num);
     div.numerator = numerator_div;
     div.denominator = denominator_div;
-    
+    if (div.denominator < 0) {
+        div.numerator = - div.numerator;
+        div.denominator = - div.denominator;
+    }
     return div;
 }
 
@@ -186,49 +231,67 @@ int main() {
         }
         
         // display results
-        // addition
-        cout << endl;
-        if (perform_addition(ratio1, ratio2).denominator == 1) {
-            cout << "a. Addition result: " << perform_addition(ratio1, ratio2).numerator << endl;
-        }
-        else if (perform_addition(ratio1, ratio2).denominator != 1) {
-            cout << "a. Addition result: " << perform_addition(ratio1, ratio2).numerator << "/" << perform_addition(ratio1, ratio2).denominator << endl;
-        }
-        
-        // subtraction
-        if (perform_subtraction(ratio1, ratio2).denominator == 1) {
-            cout << "b. Subtraction result: " << perform_subtraction(ratio1, ratio2).numerator << endl;
-        }
-        else if (perform_subtraction(ratio1, ratio2).denominator != 1){
-            cout << "b. Subtraction result: " << perform_subtraction(ratio1, ratio2).numerator << "/" << perform_subtraction(ratio1, ratio2).denominator << endl;
-        }
-        
-        // multiplication
-        if (perform_multiplication(ratio1, ratio2).denominator == 1) {
-            cout <<  "c. Multiplication result: " << perform_multiplication(ratio1, ratio2).numerator << endl;
-        }
-        else if (perform_multiplication(ratio1, ratio2).denominator != 1) {
-            cout << "c. Multiplication result: " << perform_multiplication(ratio1, ratio2).numerator << "/" << perform_multiplication(ratio1, ratio2).denominator << endl;
-        }
-        
-        // division
-        if (perform_division(ratio1, ratio2).denominator == 1 ) {
-            cout << "d. Division result: " << perform_division(ratio1, ratio2).numerator << endl;
-        }
-        else if (perform_division(ratio1, ratio2).denominator != 1 ){
-            cout << "d. Division result: " << perform_division(ratio1, ratio2).numerator << "/" << perform_division(ratio1, ratio2).denominator << endl;
-        }
-        
+        cout << fixed << setprecision(2);
         // display original valid rational numbers
-        cout << "e. Rational number 1: " << ratio1.numerator << "/" << ratio1.denominator << endl;
-        cout << "   Rational number 2: " << ratio2.numerator << "/" << ratio2.denominator << endl;
+        cout << endl;
+        cout << "a. Rational number 1: " << ratio1.numerator << "/" << ratio1.denominator;
+        cout << " (" << reduce_form(ratio1).numerator << "/" << reduce_form(ratio1).denominator << ")" << endl;
+        
+        cout << "   Rational number 2: " << ratio2.numerator << "/" << ratio2.denominator;
+        cout << " (" << reduce_form(ratio2).numerator << "/" << reduce_form(ratio2).denominator << ")" << endl;
         
         // display original rational numbers in floating-point format
         double num1 = covert_to_float(ratio1);
         double num2 = covert_to_float(ratio2);
-        cout << "f. Rational number 1 in decimal: " << num1 << endl;
+        cout << "b. Rational number 1 in decimal: " << num1 << endl;
         cout << "   Rational number 2 in decimal: " << num2 << endl;
         
+        // convert original fractions to reduced form
+        ratio1 = reduce_form(ratio1);
+        ratio2 = reduce_form(ratio2);
+        
+        // addition
+        cout << endl;
+        if (perform_addition(ratio1, ratio2).denominator == 1) {
+            cout << "c. Addition result: " << perform_addition(ratio1, ratio2).numerator << endl;
+        }
+        else if (perform_addition(ratio1, ratio2).denominator != 1) {
+            double fraction_add  = static_cast<double>(perform_addition(ratio1, ratio2).numerator) / static_cast<double>(perform_addition(ratio1, ratio2).denominator);
+            cout << "c. Addition result: " << perform_addition(ratio1, ratio2).numerator << "/" << perform_addition(ratio1, ratio2).denominator;
+            cout << " (" << round(fraction_add * 100) / 100 << ")" << endl;
+        }
+        
+        // subtraction
+        if (perform_subtraction(ratio1, ratio2).denominator == 1) {
+            cout << "d. Subtraction result: " << perform_subtraction(ratio1, ratio2).numerator << endl;
+        }
+        else if (perform_subtraction(ratio1, ratio2).denominator != 1){
+            double fraction_sub  = static_cast<double>(perform_subtraction(ratio1, ratio2).numerator) / static_cast<double>(perform_subtraction(ratio1, ratio2).denominator);
+            cout << "d. Subtraction result: " << perform_subtraction(ratio1, ratio2).numerator << "/" << perform_subtraction(ratio1, ratio2).denominator;
+            cout << " (" << round(fraction_sub * 100) / 100 << ")" << endl;
+        }
+        
+        // multiplication
+        if (perform_multiplication(ratio1, ratio2).denominator == 1) {
+            cout <<  "e. Multiplication result: " << perform_multiplication(ratio1, ratio2).numerator << endl;
+        }
+        else if (perform_multiplication(ratio1, ratio2).denominator != 1) {
+            double fraction_mul  = static_cast<double>(perform_multiplication(ratio1, ratio2).numerator) / static_cast<double>(perform_multiplication(ratio1, ratio2).denominator);
+            cout << "e. Multiplication result: " << perform_multiplication(ratio1, ratio2).numerator << "/" << perform_multiplication(ratio1, ratio2).denominator;
+            cout << " (" << round(fraction_mul * 100) / 100 << ")" << endl;
+        }
+        
+        // division
+        if (perform_division(ratio1, ratio2).denominator == 1 ) {
+            cout << "f. Division result: " << perform_division(ratio1, ratio2).numerator << endl;
+        }
+        else if (perform_division(ratio1, ratio2).denominator != 1 ){
+            double fraction_div  = static_cast<double>(perform_division(ratio1, ratio2).numerator) / static_cast<double>(perform_division(ratio1, ratio2).denominator);
+            cout << "f. Division result: " << perform_division(ratio1, ratio2).numerator << "/" << perform_division(ratio1, ratio2).denominator;
+            cout << " (" << round(fraction_div * 100) / 100 << ")" << endl;
+        }
+        
+
         // prompt user to continuously run the programe
         cout << endl << "Run again (y/n)? ";
         cin >> choice;
